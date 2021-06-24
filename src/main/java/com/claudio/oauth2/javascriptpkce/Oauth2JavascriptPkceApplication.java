e06a73c991b286852603aa175ffb1d0042009a42
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-06-24 16:35:11 -0500

    Application code structure

---
+++ src/main/java/com/claudio/oauth2/javascriptpkce/Oauth2JavascriptPkceApplication.java
@@ -0,0 +1,13 @@
+package com.claudio.oauth2.javascriptpkce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JavascriptPkceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JavascriptPkceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudio/oauth2/javascriptpkce/Oauth2JavascriptPkceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudio.oauth2.javascriptpkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JavascriptPkceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
